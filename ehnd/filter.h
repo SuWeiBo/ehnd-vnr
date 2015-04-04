da0914fbbb10956c378762329e4d745e028441f2
jichi <jichifly@gmail.com>
2015-04-04 00:50:14 -0600

    Increase encoding buffer which would crash userdict

--- ehnd/filter.h
+++ ehnd/filter.h
@@ -11,24 +11,36 @@ struct FILTERSTRUCT
   int operator<(FILTERSTRUCT fs) { return (layer) < (fs.layer) || ((layer) == (fs.layer) && (g_line < fs.g_line)); }
 };
 
+// jichi 4/3/2015: 31 is the size limited by eztrans's user-defined dictionary
+enum { userdict_text_size = 31 };
+
 struct USERDICSTRUCT
 {
   int g_line;
   int line;
   wchar_t _type;
-  wchar_t _jpn[31];
-  wchar_t _kor[31];
+  wchar_t _jpn[userdict_text_size];
+  wchar_t _kor[userdict_text_size];
   wchar_t _attr[37];
   wchar_t _db[260];
 
   int operator<(USERDICSTRUCT uds) {
-    char s1[31], s2[31];
+    // jichi 4/3/2015: The 31 limit should not work, which could become overflow.
+    // No idea why the original Ehnd works without crashing.
+    // Change buffer to 31 * 3 instead.
+    //char s1[31], s2[31];
+    enum { buffer_size = userdict_text_size * 3 };
+    char s1[buffer_size], s2[buffer_size];
 
     int len;
     len = _WideCharToMultiByte(932, 0, _jpn, -1, NULL, NULL, NULL, NULL);
+    if (len >= buffer_size)
+      len = buffer_size - 1;
     _WideCharToMultiByte(932, 0, _jpn, -1, s1, len, NULL, NULL);
     s1[len] = 0;
     len = _WideCharToMultiByte(932, 0, uds._jpn, -1, NULL, NULL, NULL, NULL);
+    if (len >= buffer_size)
+      len = buffer_size - 1;
     _WideCharToMultiByte(932, 0, uds._jpn, -1, s2, len, NULL, NULL);
     s2[len] = 0;
 
